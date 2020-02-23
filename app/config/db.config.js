ee11150b4e76e939c3ab5d302915357434510b0d
yacinezr <zribi.yacine1@gmail.com>
2020-02-23 12:20:00 +0100

    first commit

---
+++ app/config/db.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  HOST: "remotemysql.com",
+  USER: "aOw3U3Cm9o",
+  PASSWORD: "IyshbiGXEZ",
+  DB: "aOw3U3Cm9o",
+  dialect: "mysql",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+};
